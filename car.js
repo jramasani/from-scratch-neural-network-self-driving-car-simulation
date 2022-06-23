f35d54557fabc484cb754ff98804a38ca5011e6c
Jayavardhan Reddy <rsaijayavardhanreddy@gmail.com>
2022-06-24 03:20:12 +0800

    added sensors

--- car.js
+++ car.js
@@ -1,77 +1,112 @@
 class Car {
-    constructor(x,y,w,h){
-        this.x=x;
-        this.y=y; 
-        this.w=w; 
-        this.h=h;
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
 
-        this.speed = 0; 
-        this.acceleration = 0.2; 
-        this.maxSpeed = 3; 
-        this.friction = 0.05; 
-        this.angle = 0; 
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
 
-        this.controls = new Controls(); 
+
+        this.sensors = new Sensors(this);
+        this.controls = new Controls();
     }
 
-    update(){
+    update(roadBorders) {
         this.#move();
+        this.polygon = this.#createPolygon();
+        this.sensors.update(roadBorders);
     }
 
-    draw(ctx){
-        ctx.save(); 
-        ctx.translate(this.x, this.y); 
-        ctx.rotate(-this.angle); 
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
 
         ctx.beginPath()
         ctx.rect(
-            -this.w/2, 
-            -this.h/2, 
-            this.w, 
+            -this.w / 2,
+            -this.h / 2,
+            this.w,
             this.h
         )
-        ctx.fill(); 
+        ctx.fill();
 
-        ctx.restore(); 
+        ctx.restore();
+
+        this.sensors.draw(ctx);
     }
 
-    #move(){
-        if(this.controls.forward){
-            this.speed += this.acceleration; 
-        }
-        if(this.controls.reverse){
-            this.speed -= this.acceleration; 
-        }
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
 
-        if(this.speed > this.maxSpeed){
-            this. speed = this.maxSpeed; 
-        }
+        })
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+
+        })
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+
+        })
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+
+        })
         
-        if(this.speed < -this.maxSpeed/2){
-            this.speed = -this.maxSpeed/2; 
+
+
+    }
+
+    #move() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
         }
 
-        if(this.speed>0){
-            this.speed -= this.friction; 
-        } 
-        if(this.speed<0){
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
             this.speed += this.friction;
         }
-        if(Math.abs(this.speed) < this.friction ){
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
 
-        if(this.speed!=0){
-            const flip = this.speed>0?1:1; 
-            if(this.controls.left){
-                this.angle+=0.03*flip; 
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : 1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
             }
-            if(this.controls.right){
-                this.angle-=0.03*flip; 
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
             }
         }
 
-        this.x -= Math.sin(this.angle)*this.speed; 
-        this.y -= Math.cos(this.angle)*this.speed; 
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 }
--- main.js
+++ main.js
@@ -3,15 +3,20 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200; 
 
 const ctx = canvas.getContext("2d"); 
-const road = new Road(canvas.width/2, canvas.width * 0.9);
-const car = new Car(100, 100, 30, 50); 
+const road = new Road(canvas.width/2, canvas.width * 0.9, 3);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50); 
 
 animate(); 
 
 function animate(){
     canvas.height = window.innerHeight; 
-    //road.draw(ctx); 
-    car.update(); 
+
+    ctx.save(); 
+    ctx.translate(0, -car.y+canvas.height*0.7); 
+    road.draw(ctx, 5); 
+    car.update(road.borders); 
     car.draw(ctx); 
+
+    ctx.restore(); 
     requestAnimationFrame(animate); 
 }
